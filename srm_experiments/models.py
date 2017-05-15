bc82d7606f0e3e0d2149ccb0dc87a82f9b7ef583
Mike Shvartsman <ms44@princeton.edu>
2017-05-15 19:22:59 -0400

    some more models

--- srm_experiments/models.py
+++ srm_experiments/models.py
@@ -1,8 +1,17 @@
 from brainiak.funcalign.srm import SRM
 from brainiak.matnormal.dpmnsrm import DPMNSRM
+from brainiak.matnormal.covs import CovFullRankCholesky, CovAR1, CovDiagonal
 from scipy import stats
 
-models = ['srm', 'dpsrm']
+models = ['srm',
+          'dpsrm_ecm',
+          'dpsrm_ecme',
+          'dpsrm_orthos_ecm',
+          'dpsrm_orthos_ecme',
+          'dpmnsrm_orthos_ecm',
+          'dpmnsrm_orthos_ecme',
+          'dpmnsrm_orthos_ecm',
+          'dpmnsrm_orthos_ecme']
 
 def srm(train_data, test_data, n_features):
     # Z-score the data
@@ -19,8 +28,7 @@ def srm(train_data, test_data, n_features):
 
     return projected_data
 
-
-def dpsrm(train_data, test_data, n_features):
+def dpsrm_ecm(train_data, test_data, n_features):
     # Z-score the data since we're not modeling noise variance
     n = len(train_data)
     for subject in range(n):
@@ -28,8 +36,112 @@ def dpsrm(train_data, test_data, n_features):
         test_data[subject] = stats.zscore(test_data[subject], axis=1, ddof=1)
 
     model = DPMNSRM(n_features=n_features)
-    model.fit(train_data, max_iter=10)
+    model.fit(train_data, max_iter=3)
     projected_data = model.transform(test_data)
     # do not zscore outputs again though (why do this?)
     return projected_data
 
+
+def dpsrm_orthos_ecm(train_data, test_data, n_features):
+    # Z-score the data since we're not modeling noise variance
+    n = len(train_data)
+    for subject in range(n):
+        train_data[subject] = stats.zscore(train_data[subject], axis=1, ddof=1)
+        test_data[subject] = stats.zscore(test_data[subject], axis=1, ddof=1)
+
+    model = DPMNSRM(n_features=n_features, s_constraint="ortho", marg_cov=CovFullRankCholesky)
+    model.fit(train_data, max_iter=3)
+    projected_data = model.transform(test_data)
+    return projected_data
+
+
+def dpmnsrm_ecm(train_data, test_data, n_features):
+
+    n = len(train_data)
+    # no zscoring here, let the model figure out the informative voxels
+
+    model = DPMNSRM(n_features=n_features, row_cov=CovDiagonal, col_cov=CovAR1)
+    model.fit(train_data, max_iter=3)
+    projected_data = model.transform(test_data)
+    # do not zscore outputs again though (why do this?)
+    return projected_data
+
+def dpmnsrm_orthos_ecm(train_data, test_data, n_features):
+
+    n = len(train_data)
+    # no zscoring here, let the model figure out the informative voxels
+
+    model = DPMNSRM(n_features=n_features, row_cov=CovDiagonal, col_cov=CovAR1, s_constraint="ortho", marg_cov=CovFullRankCholesky)
+    model.fit(train_data, max_iter=3)
+    projected_data = model.transform(test_data)
+    # do not zscore outputs again though (why do this?)
+    return projected_data
+
+def dpsrm_ecme(train_data, test_data, n_features):
+    # Z-score the data since we're not modeling noise variance
+    n = len(train_data)
+    for subject in range(n):
+        train_data[subject] = stats.zscore(train_data[subject], axis=1, ddof=1)
+        test_data[subject] = stats.zscore(test_data[subject], axis=1, ddof=1)
+
+    model = DPMNSRM(n_features=n_features, algorithm="ECME")
+    model.fit(train_data, max_iter=3)
+    projected_data = model.transform(test_data)
+    # do not zscore outputs again though (why do this?)
+    return projected_data
+
+
+def dpsrm_orthos_ecme(train_data, test_data, n_features):
+    # Z-score the data since we're not modeling noise variance
+    n = len(train_data)
+    for subject in range(n):
+        train_data[subject] = stats.zscore(train_data[subject], axis=1, ddof=1)
+        test_data[subject] = stats.zscore(test_data[subject], axis=1, ddof=1)
+
+    model = DPMNSRM(n_features=n_features, s_constraint="ortho", marg_cov=CovFullRankCholesky, algorithm="ECME")
+    model.fit(train_data, max_iter=3)
+    projected_data = model.transform(test_data)
+    return projected_data
+
+
+def dpmnsrm_ecme(train_data, test_data, n_features):
+
+    n = len(train_data)
+    # no zscoring here, let the model figure out the informative voxels
+
+    model = DPMNSRM(n_features=n_features, row_cov=CovDiagonal, col_cov=CovAR1, algorithm="ECME")
+    model.fit(train_data, max_iter=3)
+    projected_data = model.transform(test_data)
+    # do not zscore outputs again though (why do this?)
+    return projected_data
+
+def dpmnsrm_orthos_ecme(train_data, test_data, n_features):
+
+    n = len(train_data)
+    # no zscoring here, let the model figure out the informative voxels
+
+    model = DPMNSRM(n_features=n_features, row_cov=CovDiagonal, col_cov=CovAR1, s_constraint="ortho", marg_cov=CovFullRankCholesky, algorithm="ECME")
+    model.fit(train_data, max_iter=3)
+    projected_data = model.transform(test_data)
+    # do not zscore outputs again though (why do this?)
+    return projected_data
+
+
+# def hyperalignment(train_data, test_data, n_features):
+#     from mvpa2.suite import Hyperalignment
+#     from mvpa2.datasets import Dataset
+#     # Z-score the data
+#     n = len(train_data)
+#     for subject in range(n):
+#         train_data[subject] = stats.zscore(train_data[subject], axis=1, ddof=1)
+#         test_data[subject] = stats.zscore(test_data[subject], axis=1, ddof=1)
+
+#     ds_train = [Dataset(td) for td in train_data]
+#     ds_test = [Dataset(td) for td in test_data]
+#     ha = Hyperalignment()
+#     ha.train(ds_train)
+#     projected_data = ha(ds_test)
+#     for subject in range(n):
+#         projected_data[subject] = stats.zscore(projected_data[subject], axis=1, ddof=1)
+
+#     return projected_data
