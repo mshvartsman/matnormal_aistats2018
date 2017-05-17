914b0a00d315f155fd7af9dc931de9f5602ebb5f
Narayanan Sundaram <narayanan.sundaram@intel.com>
2017-05-17 11:44:22 -0700

    ICA
    Signed-off-by: Mike Shvartsman <ms44@princeton.edu>

--- srm_experiments/models.py
+++ srm_experiments/models.py
@@ -2,6 +2,7 @@ from brainiak.funcalign.srm import SRM
 from brainiak.matnormal.dpmnsrm import DPMNSRM
 from brainiak.matnormal.covs import CovFullRankCholesky, CovAR1, CovDiagonal
 from scipy import stats
+from sklearn.decomposition import FastICA
 
 
 # ECME not done for DPSRM since all updates are analytic
@@ -12,7 +13,8 @@ models = ['srm',
           'dpmnsrm_ecm',
           'dpmnsrm_ecme',
           'dpmnsrm_orthos_ecm',
-          'dpmnsrm_orthos_ecme']
+          'dpmnsrm_orthos_ecme',
+          'ica']
 
 def srm(train_data, test_data, n_features):
     # Z-score the data
@@ -28,6 +30,22 @@ def srm(train_data, test_data, n_features):
         projected_data[subject] = stats.zscore(projected_data[subject], axis=1, ddof=1)
 
     return projected_data
+
+def ica(train_data, test_data, n_features):
+    # Z-score the data
+    n = len(train_data)
+    for subject in range(n):
+        train_data[subject] = stats.zscore(train_data[subject], axis=1, ddof=1)
+        test_data[subject] = stats.zscore(test_data[subject], axis=1, ddof=1)
+
+    model = FastICA(n_components=n_features)
+    model.fit(train_data)
+    projected_data = model.transform(test_data)
+    for subject in range(n):
+        projected_data[subject] = stats.zscore(projected_data[subject], axis=1, ddof=1)
+
+    return projected_data
+
 
 def dpsrm_ecm(train_data, test_data, n_features):
     # Z-score the data since we're not modeling noise variance
