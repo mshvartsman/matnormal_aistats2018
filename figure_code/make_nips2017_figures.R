7faddb5630e00ce04fe049cfda330b022852bd2d
Mike Shvartsman <ms44@princeton.edu>
2017-05-17 19:22:45 -0400

    updated fig code

--- figure_code/make_nips2017_figures.R
+++ figure_code/make_nips2017_figures.R
@@ -2,23 +2,41 @@ library(data.table)
 library(ggplot2)
 library(plyr)
 
-result_folders <- c('/mnt/jukebox/pniintel/cohen/ms44/nips2017_data/raider',
- '/mnt/jukebox/pniintel/cohen/ms44/nips2017_data/raider/results_bkup')
+result_folder_raider <- c('/mnt/jukebox/pniintel/cohen/ms44/nips2017_data/raider/results')
+result_folder_sherlock <- c('/mnt/jukebox/pniintel/cohen/ms44/nips2017_data/sherlock/results')
 
-res_files_complete <- dir(result_folders, full.names=T, pattern="*.csv")
+res_files_raider <- dir(result_folder_raider, full.names=T, pattern="results*")
+res_files_sherlock <- dir(result_folder_sherlock, full.names=T, pattern="results*")
 
-allres <- llply(res_files_complete, fread)
-allres <- rbindlist(allres)
+res_raider <- rbindlist(llply(res_files_raider, fread))
+res_raider[,chance_level:=1/7]
+res_sherlock <- rbindlist(llply(res_files_sherlock, fread))
+res_sherlock[,chance_level:=1/49]
+res_raider[,experiment:="raider"]
+res_sherlock[,experiment:="sherlock"]
+allres <- rbind(res_raider, res_sherlock)
 
-names_from <- c("dpmnsrm_ecm", "dpmnsrm_ecme", "srm","dpsrm_ecm")
-names_to <- c("DP-MNSRM (ECM)", "DP-MNSRM (ECME)", "SRM", "DP-SRM")
+names_from <- c("dpmnsrm_ecm",
+				"dpmnsrm_ecme",
+				"dpmnsrm_orthos_ecm",
+				"dpmnsrm_orthos_ecme",
+				"dpsrm_ecm",
+				"dpsrm_orthos_ecm",
+				"srm")
+names_to <- c("DP-MNSRM (ECM)",
+			 "DP-MNSRM (ECME)",
+			 "DP-MNSRM (ECM, Orth. S)",
+			 "DP-MNSRM (ECME, Orth. S)",
+			 "DP-SRM",
+			 "DP-SRM (Ortho. S)",
+			  "SRM")
 
 allres[,fancy_names:=mapvalues(model, names_from, names_to)]
 
 p <- ggplot(allres, aes(x=factor(features), y=mean_acc, ymin=mean_acc-std_acc, ymax=mean_acc+std_acc, group=fancy_names, fill=fancy_names)) + geom_bar(stat="identity", position=position_dodge()) +
 	geom_linerange(position=position_dodge(width=0.9), colour="gray") + theme_bw(base_size=10) + 
-	geom_abline(intercept=1/7, slope = 0, linetype='dashed') + 
-	scale_fill_brewer("Method", type = "qual") +
-	labs(x="Features", y="Accuracy", title="Classification performance, Raider") 
+	geom_hline(aes(yintercept=chance_level)) + 
+	scale_fill_brewer("Method", type = "qual") + facet_wrap(~experiment, scales="free")
+	labs(x="Features", y="Accuracy", title="Classification performance") 
 
 ggsave(filename="./classification_raider.pdf", plot=p, width=4, height=5)
