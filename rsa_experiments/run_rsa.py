d4126b1106b3d672841eb0aceaaeea36d55c21fe
Mike Shvartsman <ms44@princeton.edu>
2017-05-18 14:53:16 -0400

    store cov, go easy on nureg

--- rsa_experiments/run_rsa.py
+++ rsa_experiments/run_rsa.py
@@ -9,14 +9,9 @@ from scipy import stats
 
 import numpy as np
 
-
 def run_experiment(par, input_path, outfile_path):
     mat = loadmat('%s/data_for_experiment.mat' % input_path)
     design = mat['design']
-    n_nureg = par['n_nureg']
-    if par['method'] == 'naive' and n_nureg > 10:
-        # don't run naive multiple times, n_nureg isn't a thing
-        return
 
     # if we have diff design by subj: 
     if design.ndim == 3:
@@ -24,7 +19,7 @@ def run_experiment(par, input_path, outfile_path):
 
     data = mat['fmri'][0][par['subj_num']]
 
-    fname = "%s/results_%s_s%i_%inureg.mat" % (outfile_path, par['method'], par['subj_num'], par['n_nureg'])
+    fname = "%s/results_%s_s%i.mat" % (outfile_path, par['method'], par['subj_num'])
 
     if Path(fname).exists():
         return
@@ -35,12 +30,16 @@ def run_experiment(par, input_path, outfile_path):
         m = LinearRegression(fit_intercept=False)
         m.fit(design, data.T)
         C = np.corrcoef(m.coef_.T)
+        U = np.cov(m.coef_.T)
 
     elif par['method'] == 'brsa':
         print("Running BRSA on subject %i!" % par['subj_num'])
         data = stats.zscore(data, axis=1, ddof=1)  
+        # for brsa: 1% number of voxels (min 10)
+        n_nureg = np.max([data.shape[0] // 100, 10])
         m = BRSA(n_nureg=n_nureg)
         m.fit(X=data.T, design=design)
+        U = m.U_
         C = cov2corr(m.U_)
 
     elif par['method'] == 'mnrsa':
@@ -52,12 +51,15 @@ def run_experiment(par, input_path, outfile_path):
         n_V, n_T = data.shape
         spacecov_model = CovDiagonal(size=n_V)
         timecov_model = CovAR1(size=n_T)
+        # ballpark nureg: not as many as conditions, but a few
+        n_nureg = design.shape[1] // 3
         model = MatnormBRSA(time_noise_cov=timecov_model,
                                 space_noise_cov=spacecov_model,
                                 optimizer='L-BFGS-B', n_nureg=n_nureg)
         model.fit(data.T, design)
+        U = m.U_
         C = model.C_
 
-    savemat(fname, {'C':C, 'method':par['method'], 'subject':par['subj_num']})
+    savemat(fname, {'C':C,'U':U,'method':par['method'], 'subject':par['subj_num']})
 
     return
