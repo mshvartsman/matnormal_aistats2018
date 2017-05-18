f6378f4fa8201fce7fc7a20879ddb08c865c161e
Mike Shvartsman <ms44@princeton.edu>
2017-05-18 18:53:45 -0400

    run folds

--- srm_experiments/sherlock_s_consistency.py
+++ srm_experiments/sherlock_s_consistency.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
 
     for parnum in range(start, end):
         mypar = allpar[parnum]        
-        fname = '%s/results_s_consistency_%s_%ifeatures.csv' % (outfile_path, mypar['model'], mypar['features'])
+        fname = '%s/results_s_consistency_%s_%ifeatures_fold%i.csv' % (outfile_path, mypar['model'], mypar['features'], mypar['fold'])
         if Path(fname).exists(): 
             print("Found %s, skipping" % fname)
             continue
